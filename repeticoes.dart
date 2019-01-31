bfd72d3db74efe7aebc82d717f3a5d81732929a7
bjonnycw <36452392+bjonnycw@users.noreply.github.com>
2019-01-31 11:02:40 -0200

    Create repeticoes.dart

---
+++ repeticoes.dart
@@ -0,0 +1,21 @@
+void main() {
+	
+  for(int i = 20; i >= 10; i -= 5) { // FOR É PARA QUANTIDADE DEFINIDA
+      print(i);
+   }
+  
+  int j = 5;
+  while(j < 10){ // WHILE É PARA QUANTIDADES INDEFINIDAS
+    print(j);
+    j++;
+  }
+  
+  int k = 0;
+  do {
+    print(k);
+    k++;
+  } while (k < 5);
+  
+  
+  
+}
